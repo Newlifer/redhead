9d43915e2fd62553e688c04718694693543866b4
Aleksandr Novozhilov <alex.newlifer@gmail.com>
2017-02-03 13:19:53 +0300

    Add data clonning

--- redhead/src/kernel/types.rs
+++ redhead/src/kernel/types.rs
@@ -65,7 +65,7 @@ pub struct Rec {
 }
 
 pub fn construct_rec(format: Arc<RwLock<RecFormat>>) -> Rec {
-    let cells = format.read().unwrap().cols.iter().map(|x| x.type_).collect::<Vec<CellType>>();
+    let cells = format.read().unwrap().cols.iter().map(|x| x.type_.clone()).collect::<Vec<CellType>>();
     return Rec{guid: Uuid::new_v4(),
         cells: cells,
         format: format}
