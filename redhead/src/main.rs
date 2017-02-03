3d625275c33bf284a651d16bddfad2d27ff32234
Aleksandr Novozhilov <alex.newlifer@gmail.com>
2017-02-03 11:44:15 +0300

    Fix misstyping

--- redhead/src/main.rs
+++ redhead/src/main.rs
@@ -14,7 +14,7 @@ use std::sync::{Arc, RwLock};
 fn main() {
 
     {
-        let cell_format = ArcCellFormat{name: "id".to_string(),
+        let cell_format = CellFormat{name: "id".to_string(),
                                      type_: CellType::Int32(None)};
         let rec_format = Arc::new(RwLock::new(RecFormat{cols: vec![cell_format]}));
         let rec = construct_rec(rec_format);
